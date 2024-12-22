62cd8b684181d714998750177e0494f03a9c9a54
root <root@ip-172-31-29-38.ap-southeast-1.compute.internal>
2024-12-22 14:37:52 +0000

    my first commit

--- build.sh
+++ build.sh
@@ -1,43 +1,9 @@
 #!/bin/bash
 
-# Variables
-DOCKER_USERNAME="subha24"
-DOCKER_PASSWORD="Subha@24Seenu"
+# Define variables
 IMAGE_NAME="devops_app"
-TAG="latest" 
-DOCKERFILE_PATH="."
-REGISTRY="docker.io"
-NAME="subha"
-
-# Full image name with registry
-IMAGE="${REGISTRY}/${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}"
-
-# Login to Docker registry
-echo "Logging into Docker registry..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-if [ $? -ne 0 ]; then
-  echo "Docker login failed. Please check your credentials."
-  exit 1
-fi
+TAG="latest"
 
 # Build the Docker image
-echo "Building the Docker image..."
-docker build -t "$IMAGE" "$DOCKERFILE_PATH"
-
-if [ $? -ne 0 ]; then
-  echo "Docker build failed."
-  exit 1
-fi
-
-# Push the Docker image to the registry
-echo "Pushing the Docker image to the registry..."
-docker push "$IMAGE"
-
-if [ $? -ne 0 ]; then
-  echo "Docker push failed."
-  exit 1
-fi
-
-echo "Docker image $IMAGE successfully built and pushed!"
-
+docker build -t $IMAGE_NAME:$TAG .
+echo "Docker image $IMAGE_NAME:$TAG built successfully."
